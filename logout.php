bc6bf1fefa0325fcaa85af79214d7272d94c7555
Jamie Bower <j_bower@hotmail.co.uk>
2023-07-16 11:42:09 +1000

    fix: tidy up logout

--- logout.php
+++ logout.php
@@ -1,22 +1,7 @@
 <?php
 
 require_once './helpers/session.php';
+require_once './helpers/redirect.php';
 
 unset($_SESSION['userId']);
-
-?>
-
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Image Tools</title>
-</head>
-
-<body>
-    Logout file
-</body>
-
-</html>
+redirect('/login.php');
